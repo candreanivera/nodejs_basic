2f304b47544ef2a8f9b7727c4a6ddf659e20a08c
Cristina Andreani <candreanivera@gmail.com>
2023-06-04 18:09:39 -0700

    Addding slice file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodejs_basic
 Basic nodejs exercises
+
+slice.js --> Working with slice in a string
---
+++ slice.js
@@ -0,0 +1,12 @@
+/**Returns a portion of the array from the position indicated on slice() */
+
+let hello="Hola mundo desde Node.js";
+let final=hello.slice(11);
+
+console.log(hello);
+console.log(final);
+
+
+
+
+
